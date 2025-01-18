aedba652990a4043166cc5c5cfe407e066e30f88
not etienne <166166484+notCgu@users.noreply.github.com>
2025-01-18 14:34:03 -0800

    add updateScreen test

--- script.js
+++ script.js
@@ -10,10 +10,52 @@ var hiddenCards = [0,1,2,3,4,5,6];
 
 setUpMainMenu();
 
-
 function setUpMainMenu(){
   document.getElementById("image_1").src = "images/"+["spades","diamonds","hearts","clubs"][Math.floor(Math.random()*4)]+"/"+["A",2,3,4,5,6,7,8,9,0,"J","Q","K"][Math.floor(Math.random()*13)]+".png";
   document.getElementById("image_2").src = "images/"+["spades","diamonds","hearts","clubs"][Math.floor(Math.random()*4)]+"/"+["A",2,3,4,5,6,7,8,9,0,"J","Q","K"][Math.floor(Math.random()*13)]+".png";
   document.getElementById("image_3").src = "images/"+["spades","diamonds","hearts","clubs"][Math.floor(Math.random()*4)]+"/"+["A",2,3,4,5,6,7,8,9,0,"J","Q","K"][Math.floor(Math.random()*13)]+".png";
   document.getElementById("image_4").src = "images/"+["spades","diamonds","hearts","clubs"][Math.floor(Math.random()*4)]+"/"+["A",2,3,4,5,6,7,8,9,0,"J","Q","K"][Math.floor(Math.random()*13)]+".png";
+}
+
+function setUpGame(){
+  var remainingCards = CARDS;
+  playingField = [];
+  stock = [];
+  foundations = [0,0,0,0];
+  hiddenCards = [0,1,2,3,4,5,6];
+  trash = [];
+  for(let i = 0; i < 7; i ++){
+    playingField.push([]);
+    for(let j = 0; j <= i; j ++){
+      playingField[i].push(remainingCards.splice(Math.floor(Math.random()*remainingCards.length),1)[0]);
+    }
+  }
+  stock = remainingCards;
+  updateScreen();
+}
+
+function updateScreen(){
+  var maxLength = 0;
+  var outputStrings = [];
+  for(let i = 0; i < 7; i ++){
+    maxLength = Math.max(maxLength, playingField[i].length);
+  }
+  for(let i = 0; i < maxLength; i ++){
+    var string = "";
+    for(let j = 0; j < 7; j ++){
+      if(playingField[j].length > i){
+        if(i >= hiddenCards[j]){
+          string += playingField[j][i] + " ";
+        } else {
+          string += "XX ";
+        }
+      } else {
+        string += "   ";
+      }
+    }
+    outputStrings.push(string);
+  }
+  for(let i = 0; i < outputStrings.length; i ++){
+    console.log(outputStrings[i]);
+  }
 }
