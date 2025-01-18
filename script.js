cfaf4ee6d17db5085e7aa92d0ab0a9b7d67e7215
not etienne <166166484+notCgu@users.noreply.github.com>
2025-01-18 14:38:37 -0800

    Update script.js

--- script.js
+++ script.js
@@ -8,15 +8,6 @@ var trash = [];
 var foundations = [0,0,0,0];
 var hiddenCards = [0,1,2,3,4,5,6];
 
-setUpMainMenu();
-
-function setUpMainMenu(){
-  document.getElementById("image_1").src = "images/"+["spades","diamonds","hearts","clubs"][Math.floor(Math.random()*4)]+"/"+["A",2,3,4,5,6,7,8,9,0,"J","Q","K"][Math.floor(Math.random()*13)]+".png";
-  document.getElementById("image_2").src = "images/"+["spades","diamonds","hearts","clubs"][Math.floor(Math.random()*4)]+"/"+["A",2,3,4,5,6,7,8,9,0,"J","Q","K"][Math.floor(Math.random()*13)]+".png";
-  document.getElementById("image_3").src = "images/"+["spades","diamonds","hearts","clubs"][Math.floor(Math.random()*4)]+"/"+["A",2,3,4,5,6,7,8,9,0,"J","Q","K"][Math.floor(Math.random()*13)]+".png";
-  document.getElementById("image_4").src = "images/"+["spades","diamonds","hearts","clubs"][Math.floor(Math.random()*4)]+"/"+["A",2,3,4,5,6,7,8,9,0,"J","Q","K"][Math.floor(Math.random()*13)]+".png";
-}
-
 function setUpGame(){
   var remainingCards = CARDS;
   playingField = [];
